3e6f65f2fe8b5f9afb636cfb6c5bd185e44ba00d
antoinehage <me@antoinehage.com>
2017-09-25 15:58:15 -0400

    added reg v2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 'use strict';
+var registry_struct = {};
+var autoReloadTimeout = {};
 
 /**
  *
@@ -13,7 +15,7 @@ module.exports = function (configuration) {
      */
     function mapInjectedObject(req) {
         var input = req.headers['soajsinjectobj'];
-        if(typeof input === 'string')
+        if (typeof input === 'string')
             input = JSON.parse(input);
         if (!input)
             return null;
@@ -46,62 +48,115 @@ module.exports = function (configuration) {
                 acl_all_env: input.package.acl_all_env || null
             };
         }
-        if(input.device)
+        if (input.device)
             output.device = input.device || '';
-        if(input.geo)
+        if (input.geo)
             output.geo = input.geo || {};
-        if(input.urac)
+        if (input.urac)
             output.urac = input.urac || null;
-	
-	    if(input.awareness){
-		    output.awareness = {
-		    	host : input.awareness.host || "",
-		    	port : input.awareness.port || "",
-			    getHost : function(){
-				    var serviceName, version, env, cb;
-				    cb = arguments[arguments.length -1];
-				
-				    switch(arguments.length){
-					    //controller, cb
-					    case 2:
-						    serviceName = arguments[0];
-						    break;
-					
-					    //controller, 1, cb
-					    case 3:
-						    serviceName = arguments[0];
-						    version = arguments[1];
-						    break;
-					
-					    //controller, 1, dash, cb [dash is ignored]
-					    case 4:
-						    serviceName = arguments[0];
-						    version = arguments[1];
-						    break;
-				    }
-				
-				    var host = input.awareness.host;
-				
-				    if(serviceName && serviceName.toLowerCase() !== 'controller') {
-					    host += ":" + input.awareness.port + "/";
-					    host += serviceName;
-					
-					    if (version && !isNaN(parseInt(version))) {
-						    host += "v" + version + "/"
-					    }
-				    }
-				
-				    return cb(host);
-			    }
-		    };
-	    }
-        
+
+        if (input.awareness) {
+            output.awareness = {
+                host: input.awareness.host || "",
+                port: input.awareness.port || "",
+                getHost: function () {
+                    var serviceName, version, env, cb;
+                    cb = arguments[arguments.length - 1];
+
+                    switch (arguments.length) {
+                        //controller, cb
+                        case 2:
+                            serviceName = arguments[0];
+                            break;
+
+                        //controller, 1, cb
+                        case 3:
+                            serviceName = arguments[0];
+                            version = arguments[1];
+                            break;
+
+                        //controller, 1, dash, cb [dash is ignored]
+                        case 4:
+                            serviceName = arguments[0];
+                            version = arguments[1];
+                            break;
+                    }
+
+                    var host = input.awareness.host;
+
+                    if (serviceName && serviceName.toLowerCase() !== 'controller') {
+                        host += ":" + input.awareness.port + "/";
+                        host += serviceName;
+
+                        if (version && !isNaN(parseInt(version))) {
+                            host += "v" + version + "/"
+                        }
+                    }
+
+                    return cb(host);
+                }
+            };
+        }
+
         return output;
+    }
+
+    function execRegistry(param, cb) {
+        var err = null;
+        //TODO: this should not be equal to registry. this should be reg Functions with all the methods. like new reg(env)
+        var reg = null;
+        if (process.env.SOAJS_REGISTRY_API.indexOf(":") === -1)
+            err = new Error('Invalid format for SOAJS_REGISTRY_API [hostname:port]: ' + process.env.SOAJS_REGISTRY_API);
+
+        if (!err) {
+            var portFromEnv = process.env.SOAJS_REGISTRY_API.substr(process.env.SOAJS_REGISTRY_API.indexOf(":") + 1);
+            var port = parseInt(portFromEnv);
+            if (isNaN(port))
+                err = new Error('port must be integer: [' + portFromEnv + ']');
+        }
+
+        if (!err) {
+            var requestOption = {
+                "url": "http://" + process.env.SOAJS_REGISTRY_API + "/getRegistry?env=" + param.envCode + "&serviceName=" + param.serviceName,
+                "json": true
+            };
+            request(requestOption, function (err, response, body) {
+                if (!err) {
+                    if (body.result) {
+                        reg = body.data;
+                        registry_struct[reg.environment] = reg;
+                        if (reg && reg.serviceConfig.awareness.autoRelaodRegistry) {
+                            var autoReload = function () {
+                                execRegistry(param, function (err, reg) {
+                                    if (reg.serviceConfig.awareness.autoRelaodRegistry) {
+                                        if (!autoReloadTimeout[reg.environment])
+                                            autoReloadTimeout[reg.environment] = {};
+                                        if (autoReloadTimeout[reg.environment].timeout)
+                                            clearTimeout(autoReloadTimeout[reg.environment].timeout);
+                                        autoReloadTimeout[reg.environment].setBy = param.setBy;
+                                        autoReloadTimeout[reg.environment].timeout = setTimeout(autoReload, reg.serviceConfig.awareness.autoRelaodRegistry);
+                                    }
+                                });
+                            };
+                            if (!autoReloadTimeout[reg.environment])
+                                autoReloadTimeout[reg.environment] = {};
+                            if (autoReloadTimeout[reg.environment].timeout)
+                                clearTimeout(autoReloadTimeout[reg.environment].timeout);
+                            autoReloadTimeout[reg.environment].setBy = param.setBy;
+                            autoReloadTimeout[reg.environment].timeout = setTimeout(autoReload, reg.serviceConfig.awareness.autoRelaodRegistry);
+                        }
+                    }
+                }
+                cb(err, reg);
+            });
+        }
+        else
+            cb(err, reg);
     }
 
     return function (req, res, next) {
         if (!req.soajs)
-            req.soajs = {};
+            req.soajs = {"error":[]};
         var injectObj = mapInjectedObject(req);
         if (injectObj && injectObj.application && injectObj.application.package && injectObj.key && injectObj.tenant) {
             req.soajs.tenant = injectObj.tenant;
@@ -119,9 +174,36 @@ module.exports = function (configuration) {
             req.soajs.device = injectObj.device;
             req.soajs.geo = injectObj.geo;
             req.soajs.awareness = injectObj.awareness;
-            next();
+            if (process.env.SOAJS_REGISTRY_API && process.env.SOAJS_ENV) {
+                var param = {
+                    "envCode": process.env.SOAJS_ENV.toLowerCase(),
+                    "serviceName": req.soajs.serviceName
+
+                };
+                execRegistry(param, function (err, reg) {
+                    req.soajs.reg = reg;
+                    if (err)
+                        req.soajs.error.push (err);
+                    next();
+
+                });
+            }
         }
-        else
-            next();
+        else {
+            if (process.env.SOAJS_REGISTRY_API && process.env.SOAJS_ENV) {
+                var param = {
+                    "envCode": process.env.SOAJS_ENV.toLowerCase(),
+                    "serviceName": req.soajs.serviceName
+
+                };
+                execRegistry(param, function (err, reg) {
+                    req.soajs.reg = reg;
+                    if (err)
+                        req.soajs.error.push (err);
+                    next();
+
+                });
+            }
+        }
     };
 };
